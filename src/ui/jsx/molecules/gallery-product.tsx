a7c4f1b32bac486e943df12aee294491a29cd32c
lejdar <lejdar.dev@pm.me>
2026-02-05 23:19:59 +0100

    feat(ui): gallery product component

--- src/ui/jsx/molecules/gallery-product.tsx
+++ src/ui/jsx/molecules/gallery-product.tsx
@@ -4,20 +4,24 @@ import Image from 'next/image';
 import { cn } from 'webdev';
 import Rating from '../atoms/rating';
 import { sanitize } from '@/lib/util/sanitize';
+import BuyMenu from '../atoms/buy-menu';
 
 const style = {
   container: cn(
-    'w-60 relative',
-    'grid grid-rows-[repeat(6,auto)] grid-cols-[1fr_auto]'
+    'w-60 h-124 relative',
+    'grid grid-rows-[repeat(5,auto)_1fr] grid-cols-[1fr_auto]'
   ),
   image: cn('w-full', 'col-span-2 pb-2'),
   rating: cn('text-lg', 'absolute', 'left-2', 'top-54'),
   novat: cn('text-positive text-lg'),
   vat: cn('text-text-low text-sm'),
   menu: cn('row-span-2'),
-  availability: cn('col-span-2 py-4', 'text-sm text-center text-text-low font-semibold'),
-  name: cn('col-span-2', 'text-lg font-bold text-secondary'),
-  specs: cn('col-span-2', 'text-text-low'),
+  availability: cn(
+    'col-span-2 py-4',
+    'text-sm text-center text-text-low font-semibold'
+  ),
+  name: cn('col-span-2', 'text-lg font-bold text-secondary pb-1'),
+  specs: cn('col-span-2 h-min ', 'text-text-low text-justify line-clamp-5 '),
 };
 
 export default function GalleryProduct({ product }: { product: Product }) {
@@ -35,7 +39,7 @@ export default function GalleryProduct({ product }: { product: Product }) {
       />
       <Rating score={rating} className={style.rating} />
       <span className={style.novat}>{price.novat}</span>
-      <div className={style.menu} />
+      <BuyMenu className={style.menu} />
       <span className={style.vat}>{price.vat}</span>
       <span
         className={style.availability}
@@ -43,7 +47,7 @@ export default function GalleryProduct({ product }: { product: Product }) {
       />
       <span className={style.name}>{name}</span>
 
-      <p className={style.specs}>{spec}</p>
+      <div className={style.specs}>{spec}</div>
     </div>
   );
 }
