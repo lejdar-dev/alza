8e1fff7952d9b344ba55ed41ba3307c5583d86e1
lejdar <lejdar.dev@pm.me>
2026-02-09 02:34:28 +0100

    feat(ui): skeleton component

---
+++ src/ui/jsx/atoms/skeleton.tsx
@@ -0,0 +1,20 @@
+import { ClassNameProp, cn } from 'webdev';
+
+export const SkeletonLine = ({ className }: ClassNameProp) => {
+  return (
+    <div
+      className={cn(
+        'bg-surface w-full  h-[1.2em] my-[0.15em]',
+        'animate-pulse',
+        className
+      )}
+    />
+  );
+};
+export const SkeletonBox = ({ className }: ClassNameProp) => {
+  return (
+    <div
+      className={cn('bg-surface w-full h-full', 'animate-pulse', className)}
+    />
+  );
+};
