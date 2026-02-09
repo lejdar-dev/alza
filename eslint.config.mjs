2b29550e5501bac967ee98356b7dc5078168a67a
lejdar <lejdar.dev@pm.me>
2026-02-09 02:41:21 +0100

    chore(lint): eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,56 @@
+<<<<<<< HEAD
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+=======
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import compat from 'eslint-plugin-compat';
+import importPlugin from 'eslint-plugin-import';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
+
+function restrictServiceImports() {
+  return {
+    plugins: { import: importPlugin },
+    rules: {
+      'import/no-restricted-paths': [
+        'error',
+        {
+          zones: [
+            {
+              target: ['./src/ui/**/*', './src/lib/!(api|service)/**/*'],
+              from: './src/lib/service/**/*',
+              message:
+                'Architecture Violation: Modules should not be imported directly from /lib/services. Use /lib/api instead.',
+            },
+          ],
+        },
+      ],
+    },
+  };
+}
 
 export default eslintConfig;
