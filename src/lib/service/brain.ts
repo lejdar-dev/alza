07e77acd514ba0eab5234309523929e0f9e0e15b
lejdar <lejdar.dev@pm.me>
2026-02-09 02:50:33 +0100

    feat(lib): app interface

---
+++ src/lib/service/brain.ts
@@ -0,0 +1,18 @@
+import { Get, Result, type Paths } from 'webdev';
+import { Product, type Category } from '../data/product';
+import { Network, Server, Validation } from '../data/reason';
+
+import(`@services`) satisfies Promise<Brain>;
+export * from '@services';
+
+export interface Brain {
+  product: {
+    fetchCategories(): Promise<Result<Category[], Network | Validation>>;
+    fetchProducts(): Promise<Result<Product[], Network | Validation | Server>>;
+    fetchPopularProducts(): Promise<
+      Result<Product[], Network | Validation | Server>
+    >;
+  };
+}
+
+export type Repository<Path extends Paths<Brain>> = Get<Brain, Path>;
