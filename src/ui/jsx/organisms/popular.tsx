86b0ec723fb12ccdd48e634c325f1e3c83cc93ae
lejdar <lejdar.dev@pm.me>
2026-02-09 02:55:41 +0100

    feat(ui): popular component

--- src/ui/jsx/organisms/popular.tsx
+++ src/ui/jsx/organisms/popular.tsx
@@ -1,24 +1,38 @@
-import { usePopularProducts } from '@/lib/service/product';
+import { fetchPopularProducts } from '@/lib/api/product.api';
 import Carousel from '../molecules/carousel';
-import PopularProduct from '../molecules/popular-product';
+import PopularProduct, {
+  PopularProductSkeleton,
+} from '../molecules/popular-product';
 
-export default function Popular() {
-  const products = usePopularProducts();
+export default async function Popular() {
+  'use cache';
+  const { ok, data: products, error } = await fetchPopularProducts();
+
+  if (!ok) return 'Error occured.';
 
   return (
-    <Carousel
-      renderItem={(index) => {
-        return (
-          <PopularProduct
-            product={products.at(index % products.length)!}
-            key={products.at(index % products.length)!.id}
-          />
-        );
-      }}
-      maxItemsVisible={Math.min(10, products.length)}
-      itemWidth={60}
-      itemGap={12}
-    />
+    <Carousel itemWidth={60} itemGap={8}>
+      {products.map((product) => (
+        <PopularProduct key={product.id} product={product} />
+      ))}
+    </Carousel>
+  );
+}
+
+export function PopularSkeleton() {
+  return (
+    <Carousel itemWidth={60} itemGap={8}>
+      <PopularProductSkeleton />
+      <PopularProductSkeleton />
+      <PopularProductSkeleton />
+      <PopularProductSkeleton />
+      <PopularProductSkeleton />
+      <PopularProductSkeleton />
+      <PopularProductSkeleton />
+      <PopularProductSkeleton />
+      <PopularPritemCount itemCountoductSkeleton />
+      <PopularProductSkeleton />
+    </Carousel>
   );
 }
 
