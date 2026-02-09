038b197a1d86cf2d60c1ecb549566f8db5cbf690
lejdar <lejdar.dev@pm.me>
2026-02-09 02:50:14 +0100

    feat(lib): error reasons

---
+++ src/lib/data/reason.ts
@@ -0,0 +1,31 @@
+type Reason<Cause, Payload extends object = never> = [Payload] extends [never]
+  ? { cause: Cause }
+  : { cause: Cause } & Payload;
+
+export type Network = Reason<'network'>;
+export type Validation = Reason<'validation'>;
+export type Server<Message extends string = any> = Reason<
+  'server',
+  { message: Message }
+>;
+
+namespace Reason {
+  export const network: Network = {
+    cause: 'network',
+  };
+
+  export const validation: Validation = {
+    cause: 'validation',
+  };
+
+  export const server = <Message extends string>(
+    message: Message
+  ): Server<Message> => {
+    return {
+      cause: 'server',
+      message,
+    };
+  };
+}
+
+export default Reason;
