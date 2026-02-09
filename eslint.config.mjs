3ddb33dbf509466b22c5e182f77511ae582fb19b
lejdar <lejdar.dev@pm.me>
2026-02-09 16:48:18 +0100

    build(lint): legacy browser target

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import { defineConfig, globalIgnores } from "eslint/config";
-
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import compat from 'eslint-plugin-compat';
+import { defineConfig, globalIgnores } from 'eslint/config';
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 
 import importPlugin from 'eslint-plugin-import';
@@ -12,6 +12,9 @@ import importPlugin from 'eslint-plugin-import';
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  // Typescript
+
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
@@ -19,19 +22,15 @@ const eslintConfig = defineConfig([
     },
   },
 
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
-  ...storybook.configs["flat/recommended"]
-]);
+  // Legacy browser compatibility
+  {
+    ...compat.configs['flat/recommended'],
+    settings: {
+      polyfills: ['fetch', 'URL', 'ResizeObserver'],
+    },
+  },
 
-function restrictServiceImports() {
-  return {
+  {
     plugins: { import: importPlugin },
     rules: {
       'import/no-restricted-paths': [
@@ -48,7 +47,17 @@ function restrictServiceImports() {
         },
       ],
     },
-  };
-}
+  },
+
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+  ...storybook.configs['flat/recommended'],
+]);
 
 export default eslintConfig;
