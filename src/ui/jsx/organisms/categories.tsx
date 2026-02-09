d1117bf764ad50b5ab12efc1b933e2eca7878113
lejdar <lejdar.dev@pm.me>
2026-02-09 02:55:24 +0100

    feat(ui): categories component

--- src/ui/jsx/organisms/categories.tsx
+++ src/ui/jsx/organisms/categories.tsx
@@ -1,16 +1,25 @@
-import { useCategories } from '@/lib/service/product';
-import Category from '../atoms/category';
+import { fetchCategories } from '@/lib/api/product.api';
 import { cn } from 'webdev';
+import Category from '../atoms/category';
 
-const style = cn(`flex flex flex-wrap gap-2`);
+const style = cn(
+  `grid gap-3`,
 
-export default function Categories() {
-  const categories = useCategories();
+  'max-sm:grid-cols-[repeat(2,12rem)] max-sm:justify-center ',
+  'min-sm:grid-cols-[repeat(auto-fit,12rem)]',
+
+  'max-sm:text-sm'
+);
+
+export default async function Categories() {
+  const { ok, data: categories } = await fetchCategories();
+
+  if (!ok) throw 'Impossible state.';
 
   return (
     <div className={style}>
       {categories.map((category) => (
-        <Category key={category.label} category={category.label} />
+        <Category key={category.id} category={category.label} />
       ))}
     </div>
   );
